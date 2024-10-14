d2e54e349648192edeeb1a72e3b49e8444366209
nyadiore <robinn6@mail.uc.edu>
2024-10-13 22:24:14 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Loop that counts up by 3 from 0 to 18
+
+        for (int i = 0; i <= 18; i += 3) {
+
+            System.out.print(i + " "); // Print each number followed by a space
+
+        }
+
+    }
+}
